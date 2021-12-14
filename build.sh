55d9d735eb3760fb826390a7f6f2be58259fbbc1
Nakst <17091026+nakst@users.noreply.github.com>
2021-12-14 13:05:56 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,9 @@ COMMIT=`git log | head -n 1 | cut -b 8-14`
 mkdir -p bin
 echo accepted_license=1 >> bin/build_config.ini
 echo automated_build=1 >> bin/build_config.ini
-./start.sh
+./start.sh get-source prefix https://github.com/nakst/build-gcc-x86_64-essence/releases/download/gcc-v11.1.0/out.tar.xz
+./start.sh setup-pre-built-toolchain
+./start.sh b
 cd ..
 xz -z essence/bin/drive
 mv essence/bin/drive.xz .
