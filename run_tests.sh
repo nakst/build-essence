2c430c4f94451ae9df8b563cbdbbcf19200fa85c
Nakst <17091026+nakst@users.noreply.github.com>
2022-01-01 10:51:52 +0000

    Create run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eux
+
+git clone --depth=1 https://gitlab.com/nakst/essence.git
+cd essence
+COMMIT=`git log | head -n 1 | cut -b 8-14`
+echo "::set-output name=RELEASE_NAME::tests-`date +%Y_%m_%d`-${COMMIT}"
+
+mkdir -p bin
+echo "accepted_license=1" >> bin/build_config.ini
+echo "desktop/api_tests.ini" >> bin/extra_applications.ini
+./start.sh get-source prefix https://github.com/nakst/build-gcc/releases/download/gcc-11.1.0/gcc-x86_64-essence.tar.xz
+./start.sh setup-pre-built-toolchain
+./start.sh run-tests
+
+cd ..
+echo $COMMIT > essence/bin/commit.txt
+rm -rf essence/cross essence/.git essence/bin/cache essence/bin/freetype essence/bin/harfbuzz essence/bin/musl essence/root/Applications/POSIX/lib essence/bin/drive
