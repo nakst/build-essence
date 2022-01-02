cc640ef3b9660e08c28693b3fc7faa7285cfbe57
Nakst <17091026+nakst@users.noreply.github.com>
2022-01-02 10:17:52 +0000

    Update build_minimal.sh

--- build_minimal.sh
+++ build_minimal.sh
@@ -11,12 +11,18 @@ curl -sfLo nul https://api.github.com/repos/nakst/build-essence/releases/tags/es
 
 # Setup config files.
 mkdir -p bin root
-echo "accepted_license=1"            >> bin/build_config.ini
-echo "automated_build=1"             >> bin/build_config.ini
-echo "Flag.DEBUG_BUILD=0"            >> bin/config.ini
-echo "BuildCore.NoImportPOSIX=1"     >> bin/config.ini
-echo "BuildCore.RequiredFontsOnly=1" >> bin/config.ini
-echo "Emulator.PrimaryDriveMB=32"    >> bin/config.ini
+echo "accepted_license=1"               >> bin/build_config.ini
+echo "automated_build=1"                >> bin/build_config.ini
+echo "Flag.DEBUG_BUILD=0"               >> bin/config.ini
+echo "BuildCore.NoImportPOSIX=1"        >> bin/config.ini
+echo "BuildCore.RequiredFontsOnly=1"    >> bin/config.ini
+echo "Emulator.PrimaryDriveMB=32"       >> bin/config.ini
+echo "Emulator.PrimaryDriveMB=32"       >> bin/config.ini
+echo "Dependency.ACPICA=0"              >> bin/config.ini
+echo "Dependency.stb_image=0"           >> bin/config.ini
+echo "Dependency.stb_image_write=0"     >> bin/config.ini
+echo "Dependency.stb_sprintf=0"         >> bin/config.ini
+echo "Dependency.FreeTypeAndHarfBuzz=0" >> bin/config.ini
 
 # Setup toolchain and build the system.
 ./start.sh get-source prefix https://github.com/nakst/build-gcc/releases/download/gcc-11.1.0/gcc-x86_64-essence.tar.xz
