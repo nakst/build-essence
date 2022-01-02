122ed3e88c6b84c60a21655d33d076747572c182
Nakst <17091026+nakst@users.noreply.github.com>
2022-01-02 10:22:26 +0000

    Update build_minimal.sh

--- build_minimal.sh
+++ build_minimal.sh
@@ -4,10 +4,6 @@ set -eux
 # Get the source.
 git clone --depth=1 https://gitlab.com/nakst/essence.git
 cd essence
-COMMIT=`git log | head -n 1 | cut -b 8-14`
-
-# If this commit has already been built, exit.
-curl -sfLo nul https://api.github.com/repos/nakst/build-essence/releases/tags/essence-${COMMIT}-minimal && exit
 
 # Setup config files.
 mkdir -p bin root
@@ -28,17 +24,3 @@ echo "Dependency.FreeTypeAndHarfBuzz=0" >> bin/config.ini
 ./start.sh get-source prefix https://github.com/nakst/build-gcc/releases/download/gcc-11.1.0/gcc-x86_64-essence.tar.xz
 ./start.sh setup-pre-built-toolchain
 ./start.sh build-optimised
-cd ..
-
-# Compress the result.
-mv essence/bin/drive .
-tar -cJf drive.tar.xz drive
-echo $COMMIT > essence/bin/commit.txt
-rm -rf essence/cross essence/.git essence/bin/cache essence/bin/freetype essence/bin/harfbuzz essence/bin/musl essence/root/Applications/POSIX/lib
-tar -cJf debug_info.tar.xz essence
-
-# Set outputs for workflow.
-echo "::set-output name=OUTPUT_BINARY::drive.tar.xz"
-echo "::set-output name=DEBUG_OUTPUT_BINARY::debug_info.tar.xz"
-echo "::set-output name=RELEASE_NAME::essence-${COMMIT}-minimal"
-echo "::set-output name=COMMIT::${COMMIT}"
